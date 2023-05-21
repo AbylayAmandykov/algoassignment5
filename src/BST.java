51017ad42eaced3d6d248f1ed87ee474371a1821
Abylay <abylay0505@gmail.com>
2023-05-21 21:01:45 +0600

    docs added

--- src/BST.java
+++ src/BST.java
@@ -6,15 +6,21 @@ public class BST<K extends Comparable<K>, V> {
         private K key;
         private V val;
         private Node left, right;
+        // constructor
         public Node(K key, V val) {
             this.key = key;
             this.val = val;
         }
+        // String representation of node
         @Override
         public String toString() {
             return "{Key is: " + this.key + " " + "and value is: " + this.val + "}";
         }
     }
+    /*
+    put -  inserts a new node into the tree with the given key value pair
+    @return node
+     */
     public void put(K key, V val) {
         this.root = put(root, key, val);
         size++;
@@ -34,6 +40,10 @@ public class BST<K extends Comparable<K>, V> {
         }
         return node;
     }
+    /*
+    get - retrieves the value associated with the given key from the tree
+    @return node
+     */
     public V get(K key){
         Node node = get(root, key);
         return node != null ? node.val : null;
@@ -52,9 +62,14 @@ public class BST<K extends Comparable<K>, V> {
             return node;
         }
     }
+    // @return the number of nodes in the tree
     public int size() {
         return size;
     }
+    /*
+    delete - removes the node with the given key from the tree
+    @return node
+     */
     public void delete(K key) {
         this.root = delete(root, key);
         size--;
@@ -87,13 +102,19 @@ public class BST<K extends Comparable<K>, V> {
         }
         return node;
     }
+    // findMinNode - finding the minimum node
     private Node findMinNode(Node node) {
         if(node.left == null) {
             return node;
         }
         return findMinNode(node.left);
     }
+    /*
+    iterator -  performs an in-order traversal of the tree
+    @return list
+     */
     public Iterable<Node> iterator() {
+        // ArrayList - to contain the nodes in correct order
         ArrayList<Node> arrayList = new ArrayList<>();
         inOrderTraversal(arrayList, root);
         return arrayList;
--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,14 @@
 public class Main {
     public static void main(String[] args) {
         BST<Integer, String> tree = new BST<>();
-
+        //Inserting the elements
         tree.put(66, "Value 66");
         tree.put(55, "Value 55");
         tree.put(99, "Value 99");
         tree.put(88, "Value 88");
         tree.put(77, "Value 77");
 
-
+        //Iterating over the tree and printing the result
         Iterable it = tree.iterator();
         for(Object elem : it) {
             System.out.println(elem);
